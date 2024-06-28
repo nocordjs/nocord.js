3afc55447d38b6608b95268354653657899a64dc
provn-tq97 <vinhkn12@gmail.com>
2024-06-28 14:57:01 +0200

    add a tool

---
+++ tools/cleanup.js
@@ -0,0 +1,20 @@
+const fs = require('fs');
+const Path = require('path');
+
+const deleteFolderRecursive = (path) => {
+    if (fs.existsSync(path)) {
+        fs.readdirSync(path).forEach((file) => {
+            const curPath = Path.join(path, file);
+            if (fs.lstatSync(curPath).isDirectory()) {
+                deleteFolderRecursive(curPath);
+            } else {
+                fs.unlinkSync(curPath);
+            }
+        });
+        fs.rmdirSync(path);
+    }
+};
+
+const folder = process.argv.slice(2)[0];
+
+deleteFolderRecursive(Path.join(__dirname, '../dist', folder));
